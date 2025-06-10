796e3a8b54332940d06a45545835ea612bee895f
cabr3ra <uri.cabrera2000@gmail.com>
2025-06-10 19:27:28 +0200

    Fix: Converted initial_data.json to UTF-8 without BOM

--- royal_league/settings.py
+++ royal_league/settings.py
@@ -140,5 +140,3 @@ if not DEBUG:
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-# Temporal change to force Git commit
