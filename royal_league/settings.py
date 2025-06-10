e366cb72efa3949945d7ae45b53de00ccbfbcfee
cabr3ra <uri.cabrera2000@gmail.com>
2025-06-10 16:39:33 +0200

    Configurar STATIC_ROOT y STATICFILES_DIRS para despliegue en Render

--- royal_league/settings.py
+++ royal_league/settings.py
@@ -123,11 +123,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'royal_app/static'),
 ]
 
+if not DEBUG:
+    # Convert BASE_DIR to a string for os.path.join
+    STATIC_ROOT = os.path.join(str(BASE_DIR), 'staticfiles')
+
+    # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
+    # and renames the files with unique names for each version to support long-term caching
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
